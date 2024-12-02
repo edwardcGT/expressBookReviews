11f89767029fe16461e5dbac69c9430915e4c97c
edwardcGT <edward.chin@amway.com>
2024-12-02 01:30:26 -0500

    ok

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,10 +32,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
+    const author = req.params.author.toLowerCase();
     
     // Find the book with matching ISBN
-    const book = Object.values(books).filter(book => book.author === author);
+    const book = Object.values(books).filter(book => book.author.toLocaleLowerCase().includes(author));
     
     if (!book) {
         res.status(404).send("Error: Author not found");
@@ -46,14 +46,28 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title.toLowerCase();
+    
+    const book = Object.values(books).filter(book => book.title.toLocaleLowerCase().includes(title));
+    
+    if (!book) {
+        res.status(404).send("Error: Title not found");
+    }
+    else
+        res.send(book)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = Object.values(books).find(book => book.ISBN === isbn);
+    
+    if (!book) {
+        res.status(404).send("ISBN not found");
+    }
+    else 
+        res.send(book.reviews)
+    
 });
 
 module.exports.general = public_users;
